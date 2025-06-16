7084ff35e3582a6dba0ede58584d970a32a2bd1d
Anshul Ojha <anshulojha091@gmail.com>
2025-06-16 19:03:42 +0530

    Refactor Footer import and update font path in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,11 @@ import About from './components/About'
 import Services from './components/Services'
 import Work from './components/Work'
 import Contact from './components/Contact'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 
 function App() {
 
-    const [isDarkMode, setIsDarkMode] = useState(false)
-    useEffect(() => {
-      if(isDarkMode){
-        document.documentElement.classList.add('dark')
-        localStorage.theme = 'dark'
-      }else{
-        document.documentElement.classList.remove('dark')
-        localStorage.theme = ''
-      }
-    }, [isDarkMode])
-    
   return (<>
       <Navbar/>
       <Header />
